07520411660dc2d236c16398d31eba21f442cb57
thejoebaldwin <contact@humboldttechgroup.com>
2013-01-28 21:16:24 -0600

    More comments cosmetic

--- 01_28_13/AdditionCalculator/AdditionCalculator/AdditionViewController.m
+++ 01_28_13/AdditionCalculator/AdditionCalculator/AdditionViewController.m
@@ -17,12 +17,13 @@
 @synthesize firstNumberField, secondNumberField, thirdNumberField, totalLabel, messageLabel;
 
 
+//fires on tap gesture
 - (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
+    //tell all the textboxes to dismiss keyboards
     [firstNumberField resignFirstResponder];
     [secondNumberField resignFirstResponder];
     [thirdNumberField resignFirstResponder];
-    
 }
 
 - (void)viewDidLoad
@@ -38,26 +39,30 @@
 }
 
 - (IBAction)totalButtonClick:(id)sender {
-    
+
+    //get the user input from each textbox
     int firstNumber = [[firstNumberField text] intValue];
     int secondNumber = [[secondNumberField text] intValue] ;
     int thirdNumber = [[thirdNumberField text] intValue];
     
-    //int total = firstNumber + secondNumber + thirdNumber;
+    //call the getTotal function, passing it all three input values
     int total = [self getTotal:firstNumber withSecondNumber:secondNumber withThirdNumber:thirdNumber];
     
-    //display the total!
+    //convert the total to NSString using format string (%i for integer)
     NSString *totalString = [NSString stringWithFormat:@"%i", total];
+    //set the totalLabel label to display the total
     [totalLabel setText:totalString];
     
+    //write to log our variable values for debugging
     NSLog(@"firstNumber:%i secondNumber:%i thirdNumber:%i", firstNumber, secondNumber, thirdNumber);
     
     
        
-    
-    
+    //switch statement
+    //  1...100 will evaluate for any integer from 1 to 100 inclusive
     switch (total) {
         case 0 ... 100:
+            //set the message label according to which case we are in
             [messageLabel setText:@"That is a small number"];
             break;
         case 101 ... 200:
@@ -67,13 +72,6 @@
             [messageLabel setText:@"That is either a very large or very small number!!!"];
             break;
     }
-    
-    
-    
-    
-    
-    
-    
     
 }
 
