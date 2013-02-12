27031f84161905de671b9208c05a95d60332f1d7
thejoebaldwin <contact@humboldttechgroup.com>
2013-02-11 20:59:50 -0600

    Image Picker

---
+++ 02_11_14/Image Picker/Odometer/OdometerAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  OdometerAppDelegate.h
+//  Odometer
+//
+//  Created by student on 2/11/13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OdometerViewController;
+
+@interface OdometerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) OdometerViewController *viewController;
+
+@end
