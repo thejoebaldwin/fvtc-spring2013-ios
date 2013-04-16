9ae360f3d47bb4df9aa6775fb8f6e480bba6f567
student <student@students-iMac-36.local>
2013-04-15 19:48:08 -0500

    TabController,NavigationController,Animations

---
+++ 03_15_13/NavigationDemo/ConfigViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfigViewController.h
+//  NavigationDemo
+//
+//  Created by student on 4/15/13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigViewController : UIViewController
+
+@end
---
+++ 03_15_13/NavigationDemo/NavigationDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NavigationDemo
+//
+//  Created by student on 4/15/13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
